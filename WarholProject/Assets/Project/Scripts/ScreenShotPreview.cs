ad9a511a76856838167563c18e13385e20921552
michaelnightmare <michaelsmith0802@gmail.com>
2018-07-24 09:09:08 -0700

    7-24-18 Closer to working fully

--- WarholProject/Assets/Project/Scripts/ScreenShotPreview.cs
+++ WarholProject/Assets/Project/Scripts/ScreenShotPreview.cs
@@ -9,32 +9,41 @@ public class ScreenShotPreview : MonoBehaviour
     [SerializeField]
     GameObject canvas;
     [SerializeField]
-    Sprite defaultImage;
+    //Sprite defaultImage;
     public string[] files = null;
     int whichScreenShotIsShown = 0;
     string folderPath = "C:/Users/Michael/Documents/GitHub/WarholProject/WarholProject";
-
+    
+  
     void Start()
     {
-        canvas.GetComponent<Image>().sprite = defaultImage;
+        //canvas.GetComponent<Image>().sprite = defaultImage;
         files = Directory.GetFiles(folderPath + "/", "*.png");
-        Debug.Log(Application.persistentDataPath);
+     
         if(files.Length > 0)
         {
-            Debug.Log("here2");
+         
             GetPictureAndShowIt();
         }
-    }
-    
+        if (files.Length > 1)
+        {
 
-    void GetPictureAndShowIt()
+            DeleteImage();
+        }
+    }
+
+
+    public void GetPictureAndShowIt()
     {
-        string pathToFile = files [whichScreenShotIsShown];
+      
+        string pathToFile = files[whichScreenShotIsShown];
         Texture2D texture = GetScreenshotImage(pathToFile);
         Sprite sp = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-        Debug.Log(sp);
-        canvas.GetComponent<Image> ().sprite = sp;
-        
+    
+        GetComponent<Image> ().sprite = sp;
+        transform.root.GetComponent<MasterSpriteHandler>().screenGrab = sp;
+
+
     }
 
     Texture2D GetScreenshotImage(string filePath)
@@ -50,6 +59,17 @@ public class ScreenShotPreview : MonoBehaviour
         }
         return texture;
     }
-    
+
+    public void DeleteImage()
+    {
+        if (files.Length > 0)
+        {
+            string pathToFile = files[whichScreenShotIsShown];
+            if (File.Exists(pathToFile))
+                File.Delete(pathToFile);
+            files = Directory.GetFiles(folderPath + "/", "*.png");
+        }
+    }
+
 
 }
--- WarholProject/Assets/Project/Scripts/TakeScreenShot.cs
+++ WarholProject/Assets/Project/Scripts/TakeScreenShot.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -9,38 +10,107 @@ public class TakeScreenShot : MonoBehaviour
 
     [SerializeField]
 
-    
+    public Text countnum;
+    public Image flash;
+    public float flashA = 1;
+    public Button TakePhotoB;
+    public Image TakePhotoBArt;
+    public ScreenShotPreview previewimage;
+
+
+
+
+
+    void Start()
+    {
+        countnum.gameObject.SetActive(false);
+        TakePhotoB.enabled = true;
+        TakePhotoBArt.enabled = true;
+        
+    }
+
+    public void startCountdown()
+    {
+        countnum.gameObject.SetActive(true);
+        TakePhotoB.enabled = false;
+        TakePhotoBArt.enabled = false;
+     
+        StartCoroutine(countingDown());
+    }
+
+    IEnumerator countingDown()
+    {
+        
+        for (int i= 4; i >= 0;i--)
+        {
+            countnum.text = (i + 1).ToString();
+        yield return new WaitForSeconds(1);
+        }
+
+        countnum.gameObject.SetActive(false);
+        flash.enabled = true;
+        
+        float t = 0f;
+        while(t <.15f)
+        {
+            t += Time.deltaTime;
+            float AlphaC= Mathf.Lerp(0,flashA,t/.15f);
+            flash.color = new Color(1, 1, 1, AlphaC);
+            yield return null; 
+        }
+      
+        flash.enabled = false;
+       
+        yield return null;
+        TakePicture();
+        yield break;
+    }
+
+
+    //public void TakePicture()
+    //{
+
+    //    Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+    //    Rect rex = new Rect(40, 175, 300, 400);
+    //    //Debug.LogError("here");
+
+    //    transform.root.GetComponent<MasterSpriteHandler>().screenGrab = Sprite.Create(tex, rex, new Vector2(0.5f, 0.5f), 100.0f);
+
+
+    //    transform.root.GetComponent<MasterSpriteHandler>().ToggleToPreview();
+    //    TakePhotoB.enabled = true;
+    //    TakePhotoBArt.enabled = true;
+
+
+
+    //}
+
 
 
     public void TakePicture()
     {
-       Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
-        Rect rex = new Rect(40, 175, 300, 400);
-        //Debug.LogError("here");
-
-        transform.root.GetComponent<MasterSpriteHandler>().screenGrab = Sprite.Create(tex, rex, new Vector2(0f, 0f), 100.0f);
-        transform.root.GetComponent<MasterSpriteHandler>().ToggleToPreview();
-
-     
+        StartCoroutine("Capture");
     }
 
+    IEnumerator Capture()
+    {
 
 
-    //public void TakeShot()
-    //{
-    //    StartCoroutine("Capture");
-    //}
-    
-    //IEnumerator Capture()
-    //{
-    //    string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
-    //    string fileName = "Screenshot" + timeStamp + ".png";
-    //    string pathToSave = fileName;
-      
-    //    ScreenCapture.CaptureScreenshot(pathToSave);
-    //    yield return new WaitForEndOfFrame();
-        
-    //}
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "Screenshot" + timeStamp + ".png";
+        string pathToSave = fileName;
+
+        ScreenCapture.CaptureScreenshot(pathToSave);
+
+        yield return new WaitForEndOfFrame();
+
+        transform.root.GetComponent<MasterSpriteHandler>().ToggleToPreview();
+
+        TakePhotoB.enabled = true;
+        TakePhotoBArt.enabled = true;
+
+    }
 
 
 }
